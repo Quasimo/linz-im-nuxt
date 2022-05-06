a84a672165c0cc42058e6684154354365234287c
Quasimo <linuximo@gmail.com>
2022-05-06 11:33:40 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <h2>{{subtitle}}</h2>
+    <p>{{description}}</p>
+    <a v-if="email" :href="`mailto:${email}`">{{email}}</a>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'Lin Zhizhao',
+      subtitle: '隐藏人物睡不着',
+      description: '日常互联网发现，随缘摄影分享。',
+      email: 'quasimo@qq.com'
+    }
+  }
+}
+</script>
